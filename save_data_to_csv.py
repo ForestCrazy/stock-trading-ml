532d32b3b54d14d1b71a32cbd602482015306362
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-04-08 13:54:25 +0700

    save_data argparse

--- save_data_to_csv.py
+++ save_data_to_csv.py
@@ -39,38 +39,39 @@ if __name__ == "__main__":
     save_dataset(**vars(namespace))
 """
 
-def crypto_data(symbol):
+def crypto_data(symbol, start_year, end_year):
     data_list = []
 
-    for j in range(1, 12+1):
-        _, num_days = calendar.monthrange(2020, j)
-        for day in range(1, num_days+1):
-            for hour in range(1, 23):
-                start_time = int(time.mktime(datetime.datetime(2020, j, day, hour, 0+1, 0).timetuple())) * 1000
-                end_time = int(time.mktime(datetime.datetime(2020, j, day, hour+1, 0, 0).timetuple())) * 1000
-                url = "https://api2.binance.com/api/v3/klines?symbol=" + symbol + "&interval=1m&limit=1000&startTime=" + str(start_time) + "&endTime=" + str(end_time)
-                print(url)
-                response = ''
-                while response == '':
-                    try:
-                        response = requests.get(url, headers={'Cache-Control': 'no-cache'})
-                        for i in response.json():
-                            print(datetime.datetime.fromtimestamp(int(i[0]) / 1000).strftime('%Y-%m-%d %H:%M:%S'))
-                            print('Open : ', i[1])
-                            print('Close : ', i[4])
-                            print('High : ', i[2])
-                            print('Low : ', i[3])
-                            print('Volume : ', i[5])
-                            print('\n')
-                            data_list.insert(0, [datetime.datetime.fromtimestamp(int(i[0]) / 1000).strftime('%Y-%m-%d %H:%M:%S'), i[1], i[4], i[2], i[3], i[5]])
-                        break
-                    except:
-                        print("Connection refused by the server..")
-                        print("Let me sleep for 5 seconds")
-                        print("ZZzzzz...")
-                        time.sleep(2)
-                        print("Was a nice sleep, now let me continue...")
-                        continue
+    for year in range(start_year, end_year):
+        for month in range(1, 12+1):
+            _, num_days = calendar.monthrange(year, month)
+            for day in range(1, num_days+1):
+                for hour in range(1, 23):
+                    start_time = int(time.mktime(datetime.datetime(year, month, day, hour, 0+1, 0).timetuple())) * 1000
+                    end_time = int(time.mktime(datetime.datetime(year, month, day, hour+1, 0, 0).timetuple())) * 1000
+                    url = "https://api2.binance.com/api/v3/klines?symbol=" + symbol + "&interval=1m&limit=1000&startTime=" + str(start_time) + "&endTime=" + str(end_time)
+                    print(url)
+                    response = ''
+                    while response == '':
+                        try:
+                            response = requests.get(url, headers={'Cache-Control': 'no-cache'})
+                            for i in response.json():
+                                print(datetime.datetime.fromtimestamp(int(i[0]) / 1000).strftime('%Y-%m-%d %H:%M:%S'))
+                                print('Open : ', i[1])
+                                print('Close : ', i[4])
+                                print('High : ', i[2])
+                                print('Low : ', i[3])
+                                print('Volume : ', i[5])
+                                print('\n')
+                                data_list.insert(0, [datetime.datetime.fromtimestamp(int(i[0]) / 1000).strftime('%Y-%m-%d %H:%M:%S'), i[1], i[4], i[2], i[3], i[5]])
+                            break
+                        except:
+                            print("Connection refused by the server..")
+                            print("Let me sleep for 5 seconds")
+                            print("ZZzzzz...")
+                            time.sleep(2)
+                            print("Was a nice sleep, now let me continue...")
+                            continue
 
     ## init csv file
     a = numpy.array([['date','1. open','2. high','3. low','4. close','5. volume']])
@@ -89,6 +90,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument('symbol', type=str, help="the stock symbol you want to download")
+    parser.add_argument('start_year', type=str, help="the start year kline data you want to download")
+    parser.add_argument('end_year', type=str, help="the end year kline data you want to download")
 
     namespace = parser.parse_args()
     crypto_data(**vars(namespace))
