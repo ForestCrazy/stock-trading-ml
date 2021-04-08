7f4493b388854cce132ca15f725878acb6a4922f
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2021-04-08 15:14:14 +0700

    add notification(win10toast) to alert task

--- save_data_to_csv.py
+++ save_data_to_csv.py
@@ -5,6 +5,8 @@ import time
 import csv
 import numpy
 
+from win10toast import ToastNotifier
+
 from alpha_vantage.timeseries import TimeSeries
 from pprint import pprint
 import json
@@ -40,10 +42,12 @@ if __name__ == "__main__":
 """
 
 def crypto_data(symbol, start_year, end_year):
+    toaster = ToastNotifier()
     data_list = []
 
     for year in range(start_year, end_year):
         for month in range(1, 12+1):
+            toaster.show_toast("Crypto Price Predicted Project", f"start load kline data at {month}/{year}.", icon_path=None, duration=10)
             _, num_days = calendar.monthrange(year, month)
             for day in range(1, num_days+1):
                 for hour in range(1, 23):
@@ -72,6 +76,7 @@ def crypto_data(symbol, start_year, end_year):
                             time.sleep(2)
                             print("Was a nice sleep, now let me continue...")
                             continue
+        toaster.show_toast("Crypto Price Predicted Project", f"success load kline data of year {year}.", icon_path=None, duration=10)
 
     ## init csv file
     a = numpy.array([['date','1. open','2. high','3. low','4. close','5. volume']])
@@ -90,8 +95,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument('symbol', type=str, help="the stock symbol you want to download")
-    parser.add_argument('start_year', type=str, help="the start year kline data you want to download")
-    parser.add_argument('end_year', type=str, help="the end year kline data you want to download")
+    parser.add_argument('start_year', type=int, help="the start year kline data you want to download")
+    parser.add_argument('end_year', type=int, help="the end year kline data you want to download")
 
     namespace = parser.parse_args()
     crypto_data(**vars(namespace))
